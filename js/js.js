54945322f6b29969d798b79b93d8104f12e19864
Elias Mari <elias.mari.p@gmail.com>
2024-10-29 19:17:37 +0100

    funciones que voy a utilizar

--- js/js.js
+++ js/js.js
@@ -13,10 +13,17 @@ function comenzar(){
 
 function comprobar() {
     if(fila()){
+
+        ventana();
         
+
     } else if (columna()) {
+
+        ventana();
         
     } else if (diagonal()) {
+
+        ventana();
         
     }
 }
@@ -31,4 +38,13 @@ function columna(){
 
 function diagonal(){
     
+}
+
+function ventana(){
+    ventana = window.open("", "_blank", "width=200 , height=100, top=50, left=50");
+        ventana.document.write("GANADOR JUGADOS: " + jugador);
+        setTimeout(() => {
+            ventana.close();
+            comenzar();
+        }, 3000);
 }
