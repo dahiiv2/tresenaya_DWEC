bd7c652bc2aea4c7b9127b62fee386727df9d28c
Elias Mari <elias.mari.p@gmail.com>
2024-10-30 19:44:31 +0100

    comprobacion partida

--- js/js.js
+++ js/js.js
@@ -118,9 +118,62 @@ function comprobar() {
 
 function fila(){
     const fila1 = [1, 4, 7];
+    const fila2 = [2, 5, 8];
+    const fila3 = [3, 6, 9];
 
+    let resultado1 = verificar(fila1);
+    let resultado2 = verificar(fila2);
+    let resultado3 = verificar(fila3);
+
+    if (resultado1 || resultado2 || resultado3) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function columna(){
+    const columna1 = [1, 2, 3];
+    const columna2 = [4, 5, 6];
+    const columna3 = [7, 8, 9];
+
+    let resultado1 = verificar(columna1);
+    let resultado2 = verificar(columna2);
+    let resultado3 = verificar(columna3);
+
+    if (resultado1 || resultado2 || resultado3) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function diagonal(){
+    const diagonal1 = [1, 5, 9];
+    const diagonal2 = [3, 5, 7];
+
+    let resultado1 = verificar(diagonal1);
+    let resultado2 = verificar(diagonal2);
+
+    if (resultado1 || resultado2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function mostrarVentana(mensaje){
+    ventana = window.open("", "_blank", "width=200 , height=100, top=50, left=50");
+        ventana.document.write(mensaje);
+        setTimeout(() => {
+            ventana.close();
+            comenzar();
+        }, 3000);
+}
+
+function verificar(fila) {
     // Seleccionar los elementos con los IDs especificados
-    const elementosSeleccionados = fila1.map(id => document.getElementById(id));
+    const elementosSeleccionados = fila.map(id => document.getElementById(id));
 
     // Crear un array para almacenar las clases de los hijos
     const clasesHijos = [];
@@ -140,30 +193,10 @@ function fila(){
         }
     });
 
-    if (clasesHijos.length > 0 && clasesHijos.every(clase => clase === clasesHijos[0])) {
+    // Comprobar si todas las clases son iguales
+    if (clasesHijos.length == 3 && clasesHijos.every(clase => clase === clasesHijos[0])) {
         return true;
     } else {
         return false;
     }
-    
-
-    const fila2 = [2, 5, 8];
-    const fila3 = [3, 6, 9];
-}
-
-function columna(){
-
-}
-
-function diagonal(){
-    
-}
-
-function mostrarVentana(mensaje){
-    ventana = window.open("", "_blank", "width=200 , height=100, top=50, left=50");
-        ventana.document.write(mensaje);
-        setTimeout(() => {
-            ventana.close();
-            comenzar();
-        }, 3000);
 }
